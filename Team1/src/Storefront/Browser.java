51d61ed5bbafb901e7a278fd3d11efad9b20d6fd
Sean <seb1182@unh.edu>
2024-03-14 21:35:08 -0400

    editing comments

--- Team1/src/Storefront/Browser.java
+++ Team1/src/Storefront/Browser.java
@@ -24,12 +24,12 @@ public class Browser extends JFrame {
         for (int i = 0; i < 40; i++) {
             listings_array[i] = new Listing("Listing " + i);
             browser_panel.add(listings_array[i]); // add each listing panel to application
-            listings_array[i].setLocation(0, 300 * i); // set location to avoid overlap (200 is height of each listing)
+            listings_array[i].setLocation(0, 300 * i); // set location to avoid overlap (300 is height of each listing)
             browser_panel.revalidate(); // refresh the layout
         }
         
         // set expected total height of panels. this allows the scroll bar to function
-        // 200 is the pre-defined height of each listing panel
+        // 300 is the pre-defined height of each listing panel
         int panelHeight = (listings_array.length * 300) + 10;
         browser_panel.setPreferredSize(new Dimension(0, panelHeight));
         
